fa638573d4fe7e5da809366874d001f32b4b48ec
Monika Balandyte <monika.balandyte@companial.com>
2023-04-25 15:56:00 +0300

    Create POM structure. Add Driver and Base classes.

--- Framework/Driver.cs
+++ Framework/Driver.cs
@@ -1,7 +1,34 @@
 ﻿
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+
 namespace Framework
 {
-    internal class Driver
-    {
-    }
+        public class Driver
+        {
+            private static IWebDriver driver;
+
+            public static void SetupDriver()
+            {
+                driver = new ChromeDriver();
+            }
+
+            public static IWebDriver GetDriver()
+            {
+                return driver;
+            }
+
+            public static void OpenURL(string url)
+            {
+                //su refresh
+                driver.Url = url;
+                //be refresh, išlaikom veiksmus, kurie galėjo būti atlikti svetainėje
+                // driver.Navigate().GoToUrl(url);
+            }
+
+            public static void CloseDriver()
+            {
+                driver.Quit();
+            }
+        }
 }
---
+++ Tests/BaseTests.cs
@@ -0,0 +1,23 @@
+﻿
+using Framework;
+using NUnit.Framework;
+
+namespace Tests
+{
+    internal class BaseTests
+    {
+        [SetUp]
+
+        public void SetUp()
+        {
+            Driver.SetupDriver();
+        }
+
+        [TearDown]
+
+        public void TearDown()
+        {
+            Driver.CloseDriver();
+        }
+    }
+}
